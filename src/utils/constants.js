ce4d0de4ce63abf0f4086f8b7effa8aa08c3e700
Abhishek Gupta <bsaagupta77@gmail.com>
2024-01-24 17:30:15 +0530

    change proxy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const LOGO_URL =
 "https://img.freepik.com/free-vector/delivery-service-illustrated_23-2148505081.jpg?size=626&ext=jpg";
 
 export const MENU_API =
-"https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
+"https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
 
 
 
